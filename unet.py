17c0da40ae3f7c3dc6f5125108967a09e301b284
prateek <pbhatt110@gmail.com>
2020-06-23 23:07:00 +0200

    Add first basic commit.

---
+++ unet.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+
+class UNet(tf.keras.Model):
+
+    def __init__(self):
+        super(UNet, self).__init__()
+        # encoder layers
+
+        # decoder layers
+
+    def call(self, inputs):
+        pass
+
+
+model = UNet()
